342d7a9d0bbfb014bdb7e62d742c9af13ffeaacc
David.Kalacska <David.Kalacska@DKALACSKALTW10>
2018-06-26 11:53:00 +0200

    Add project files.

---
+++ AdapterSwitcher/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdapterSwitcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                Task TaskOne = Task.Factory.StartNew(() => DisableAdapter("Ethernet"));
+                TaskOne.Wait();
+                Task TaskTwo = Task.Factory.StartNew(() => EnableAdapter("Wi-Fi"));
+                TaskTwo.Wait();
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        static void EnableAdapter(string interfaceName)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo("netsh", "interface set interface \"" + interfaceName + "\" enable");
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+            Process p = new Process();
+            p.StartInfo = psi;
+            p.Start();
+        }
+
+        static void DisableAdapter(string interfaceName)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo("netsh", "interface set interface \"" + interfaceName + "\" disable");
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+            Process p = new Process();
+            p.StartInfo = psi;
+            p.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Task TaskOne = Task.Factory.StartNew(() => DisableAdapter("Wi-Fi"));
+                TaskOne.Wait();
+                Task TaskTwo = Task.Factory.StartNew(() => EnableAdapter("Ethernet"));
+                TaskTwo.Wait();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
